9844ea3ee434bc78e08d5bc674f939e07c2b9dff
twentieth <nacoipoco@gmail.com>
2017-12-05 22:02:04 +0100

    formatowanie contact.blade

--- resources/views/photos/contact.blade.php
+++ resources/views/photos/contact.blade.php
@@ -1,13 +1,13 @@
-@extends('photos.base')
+@extends ('photos.base')
   
-@section('content')
+@section ('content')
 
 <div class="w3-row">
 <div class="w3-rest">
 <div class="w3-container w3-padding-xxlarge w3-xlarge" id="contact-form">
 
-@include('photos.sidenav')
-@include('photos.messages')
+@include ('photos.sidenav')
+@include ('photos.messages')
 
 <div class="w3-row">
   <h2 class="w3-xxxlarge">send a message</h2>
@@ -15,22 +15,22 @@
 
   {{ Form::open(['url' => '/ag-photography/contact', 'method' => 'POST', 'name' => 'contact']) }}
   {{ Form::label('your_name', 'your name', ['class' => 'w3-label w3-text-black w3-hover-text-white']) }}
-  @if($errors->has('your_name'))
-    @foreach($errors->get('your_name') as $error)
+  @if ($errors->has('your_name'))
+    @foreach ($errors->get('your_name') as $error)
       <span class="w3-medium w3-text-red errors">{{ $error }} </span>
     @endforeach
   @endif
   {{ Form::text('your_name', null, ['class' => 'w3-input', 'placeholder' => '*required']) }}
   {{ Form::label('your_email', 'your e-mail', ['class' => 'w3-label w3-text-black w3-hover-text-white']) }}
-  @if($errors->has('your_email'))
-    @foreach($errors->get('your_email') as $error)
+  @if ($errors->has('your_email'))
+    @foreach ($errors->get('your_email') as $error)
       <span class="w3-medium w3-text-red errors">{{ $error }} </span>
     @endforeach
   @endif
   {{ Form::email('your_email', null, ['class' => 'w3-input', 'placeholder' => '*required']) }}
   {{ Form::label('your_message', 'your message', ['class' => 'w3-label w3-text-black w3-hover-text-white']) }}
-  @if($errors->has('your_message'))
-    @foreach($errors->get('your_message') as $error)
+  @if ($errors->has('your_message'))
+    @foreach ($errors->get('your_message') as $error)
       <span class="w3-medium w3-text-red errors">{{ $error }} </span>
     @endforeach
   @endif
@@ -39,8 +39,8 @@
   {{ Form::checkbox('cc_myself', True, True, ['class' => 'w3-check']) }}
   <br>
   <br>
-  @if($errors->has('g-recaptcha-response'))
-    @foreach($errors->get('g-recaptcha-response') as $error)
+  @if ($errors->has('g-recaptcha-response'))
+    @foreach ($errors->get('g-recaptcha-response') as $error)
       <span class="w3-medium w3-text-red errors">{{ $error }} </span>
     @endforeach
   @endif
@@ -54,4 +54,4 @@
 </div>
 </div>
 
-@endsection('content')
+@endsection ('content')
